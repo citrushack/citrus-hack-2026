b07179b539bd1269b55774bde7b4f4758876b148
Yubin Zhen <yubinzhen020105@gmail.com>
2026-04-16 15:05:13 -0700

    fixed track

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -36,6 +36,24 @@ const Tracks = () => {
     track15,
   ];
 
+  const trackImagesMobile = [
+    track2,
+    track1,
+    track3,
+    track4,
+    track5,
+    track6,
+    track7,
+    track8,
+    track9,
+    track10,
+    track11,
+    track12,
+    track14,
+    track13,
+    track15,
+  ];
+
   return (
     <div className="flex w-full flex-col items-center justify-center py-10">
       <div className="lg:mb-20">
@@ -46,8 +64,31 @@ const Tracks = () => {
         />
       </div>
 
-      <div className="mt-10 grid w-full max-w-4xl grid-cols-1 items-start gap-x-4 gap-y-2 px-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-10 hidden w-full max-w-4xl items-start gap-x-4 gap-y-2 px-4 lg:grid lg:grid-cols-3">
         {trackImages.map((src, index) => {
+          if (!src) return <div key={index} className="hidden lg:block" />;
+
+          const isCenterColumn = index % 3 === 1;
+
+          return (
+            <div
+              key={index}
+              className={` ${`mx-auto ${isCenterColumn ? "translate-y-0 lg:-translate-y-24" : ""}`}`}
+            >
+              <Image
+                src={src}
+                alt={`Track ${index + 1}`}
+                width={280}
+                height={180}
+                className="duration-200 hover:scale-105"
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="mt-10 grid w-full max-w-4xl grid-cols-1 items-start gap-x-4 gap-y-2 px-4 md:grid-cols-2 lg:hidden">
+        {trackImagesMobile.map((src, index) => {
           if (!src) return <div key={index} className="hidden lg:block" />;
 
           const isCenterColumn = index % 3 === 1;
