eed0771038e0324784de95de0ac6dd92f3f65e37
tingtingtingtin <twu062604@gmail.com>
2026-04-17 20:26:38 -0700

    fix: pass schedule as child w/ suspense to landing, event null safety

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -1,6 +1,6 @@
 import Landing from "./landing";
+import Schedule from "./schedule";
 // import About from "./about";
-// import Schedule from "./schedule";
 // import Tracks from "./tracks";
 // import Sponsors from "./sponsors";
 // import Team from "./team";
@@ -8,11 +8,22 @@ import Landing from "./landing";
 // import Judges from "./judges";
 // import FAQ from "./faq";
 // import Footer from "./footer";
+import { Suspense } from "react";
 
 const Live = () => {
   return (
     <>
-      <Landing />
+      <Landing>
+        <Suspense
+          fallback={
+            <div className="flex flex-row justify-center p-10 text-lg font-semibold text-white">
+              Loading...
+            </div>
+          }
+        >
+          <Schedule />
+        </Suspense>
+      </Landing>
       {/* <About />
       <Tracks />
       <Schedule />
